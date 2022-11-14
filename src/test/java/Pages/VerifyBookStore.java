ae289e268851a27c64bbd9490756c4ae5f2173ce
SukreetiSharma <sukreeti.sharma@infostride.com>
2022-11-14 21:18:00 +0530

    Verification

--- src/test/java/Pages/VerifyBookStore.java
+++ src/test/java/Pages/VerifyBookStore.java
@@ -23,6 +23,7 @@ public class VerifyBookStore {
     By BookStore = By.xpath("//div[@class='main-header']");
     String Row ="//a[contains(text(),'%s')]";
     String Name = "//div[contains(text(),'%s')]";
+    String authorName = "(//div[contains(text(),\"%s\")])[1]";
     public static Properties prop;
     WebDriver driver;
     WebDriverWait wait;
@@ -81,11 +82,11 @@ public class VerifyBookStore {
         System.out.println(Title);
         System.out.println(Author);
         System.out.println(Publisher);
-        String actual5 = driver.findElement(By.xpath(String.format(Row, HomePage.TITLE.getResourcesName()))).getText();
+        String actual5 = driver.findElement(By.xpath(String.format(Row, Title))).getText();
         Assert.isTrue(actual5.equals(Title), "Expected result does not match with actual result");
-        String actual6 = driver.findElement(By.xpath(String.format(Name, HomePage.AUTHOR.getResourcesName()))).getText();
+        String actual6 = driver.findElement(By.xpath(String.format(Name, Author))).getText();
         Assert.isTrue(actual6.equals(Author), "Expected result does not match with actual result");
-        String actual7 = driver.findElement(By.xpath(String.format(Name, HomePage.PUBLISHER.getResourcesName()))).getText();
+        String actual7 = driver.findElement(By.xpath(String.format(authorName, Publisher))).getText();
         Assert.isTrue(actual7.equals(Publisher), "Expected result does not match with actual result");
     }
 }
